843a2dc4f61958fc82989545f6571e002d5747e1
Deva Nand <115862793+Deva-nand07@users.noreply.github.com>
2025-06-23 12:00:35 +0530

    functions in jS

--- script.js
+++ script.js
@@ -1,149 +1,55 @@
-// console.log() stement
-console.log("\n");
-console.log("Hello World!");
-// Variablles and Data types in JS
-let fullName = "Deva nand";
-let age = 21;
-let isResult = true;
-let Variables;
-console.log(
-  "My Name is " +
-    fullName +
-    "\nand my age is " +
-    age +
-    "\nand I am " +
-    isResult +
-    " in my exams."
-);
-console.log(
-  `My Name is ${fullName} and my age is ${age} and I am ${isResult} in my exams.`
-);
-console.log(Variables); // Undefined variable
-const PI = 3.14; // Constant variable
-// Data types in JS
-let name = "Deva nand"; // String
-let x = null; // Null
-let y = undefined; // Undefined
-console.log(typeof name, typeof age, typeof isResult, typeof x, typeof y); // String
-// Objects in JS
-let person = {
-  "Full name": "Deva nand", // key name with space
-  age: 21,
-  cgpa: 9.0,
-  isPass: true,
-};
-console.log(person); // Object
-console.log(person["Full name"]); // Correct way to access
-console.log(person.age); // Correct way to access
-person.age = person.age + 1; // Update value
-person.cgpa = 9.5; // Update value
-person.salary = "10M $"; // Add new key value pair
-console.log(person);
-// Creating a Constant Object
-const student = {
-  name: "Abhay",
-  age: 21,
-  cgpa: 8.0,
-};
-student.cgpa = 7.0; // Update value of Constant Object
-student.isPass = true; // Add new key value pair
-console.log(student); // constant Object
-
-let a = "var";
-a = a + 1; // String concatenation
-console.log(a); // String concatenation
-
-const product = {
-  name: "Parker Jotter Standard CT Ballpoint Pen",
-  color: "Black",
-  rating: 4.0,
-  price: 270,
-};
-
-// Operators in JS
-let x1 = 10;
-let y1 = 20;
-console.log("x1 + y1 =", x1 + y1); // Addition
-console.log("2^3 =", 2 ** 3); // Exponentiation
-let no1 = 5; // Number
-let no2 = "5"; // String --> number 5 in string format
-console.log("no1 =", no1, "no2 = ", no2);
-console.log("no1 == no2", no1 == no2); // Loose equality (==) - checks value only
-console.log("no1 === no2", no1 === no2); // Strict equality (===) - checks value and type
-
-// conditionals in JS
+// Functions in JS
 {
-  // if statement
-  let age = 14;
-  // if else statement
-  if (age > 18) {
-    console.log("You are eligible to vote.");
-  } else {
-    console.log("You are not eligible to vote.");
-  }
-  // if else if statement
-  let marks = 90; // Number
-  if (marks >= 90) {
-    console.log("Grade A+");
-  } else if (marks >= 80) {
-    console.log("Grade A");
-  } else if (marks >= 70) {
-    console.log("Grade B+");
-  } else if (marks >= 60) {
-    console.log("Grade B");
-  } else if (marks >= 50) {
-    console.log("Grade C+");
-  } else if (marks >= 40) {
-    console.log("Grade C");
-  } else {
-    console.log("Fail");
-  }
-  // ternary operator
-  let no = 10; // Number
-  let result = no % 2 == 0 ? "Even" : "Odd"; // Even or Odd
-  console.log("The number is ", result); // Even or Odd
+  // Default functions
+  console.log("\n");
+
+  console.log("deva nand".toUpperCase()); // Convert string to uppercase
+
+  array = [1, 2, 3, 4];
+  array.push(5); // Add 5 to array
+  console.log(array); // Print array
 }
 
-// switch (key) {
-//   case value:
-//     break;
-
-//   default:
-//     break;
-// }
-
-// let Name = prompt("Enter your name: "); // Prompt user for input
-// alert("Hello " + Name); // Alert user with name
-
-// Loops
 {
-  for (let i = 1; i <= 5; i++) {
-    console.log("Hello World " + i); // Print Hello World 5 times
-  }
-  let count = 1; // Number
-  while (count <= 5) {
-    console.log(count); // Print 1 to 5
-    count++; // Increment count
-  }
+  // User defined functions
+  // function syntax
   console.log("\n");
-  do {
-    console.log(count);
-    count++; // Increment count
-  } while (count <= 5);
-  console.log("\n");
+  function name(params) {}
+  function greeting(Name) {
+    console.log("Hello " + Name); // Print Hello + Name
+  }
+  greeting("Deva nand"); // Call function with name Deva nand
+  greeting("Lalit"); // Call function with name Lalit
 
-  // for of loop
-  let size = 0;
-  for (const element of "Deva nand") {
-    console.log(element); // Print each character of string
-    size++; // Increment size
+  function add(a, b) {
+    return a + b; // Return sum of a and b
   }
-  console.log("Size of string is ", size); // Print size of string
+  sum = add(10, 20); // Call function with a = 10 and b = 20
+  console.log("Sum is ", sum); // Print sum
 
-  console.log("\n");
-  // for in loop
-  for (const key in person) {
-    const element = person[key]; // Access each key value pair of object
-    console.log(key, ":-", element); // Print each key value pair of object
+  function sub(a, b) {
+    return a - b; // Return difference of a and b
   }
+  subtraction = sub(20); // Call function with a = 20 and b = 10
+  console.log("Subtraction is ", subtraction); // Print subtraction
+  // NaN is not a number
+
+  function Exponentiation(base, exponent) {
+    let result = base ** exponent; // Exponentiation
+    return result; // Return result
+  }
+  console.log("Exponentiation of 2^3 is ", Exponentiation(2, 3)); // Call function with base = 2 and exponent = 3
+
+  // Arrow function
+  const Addition = (a, b) => {
+    return a + b; // Return sum of a and b
+  };
+  console.log("Value of Addition (arrow function) =", Addition(18, 22)); // Call function with a = 18 and b = 22
+
+  // functiojn with optional parameters
+  function salaryIncrement(salary, increment = 5000) {
+    return salary + increment; // Return salary + increment
+  }
+  console.log("Previous Salart of Deva nand = ", 100000, "$"); // Print salary of Deva nand
+  console.log("Updated Salary  of Deva nand", salaryIncrement(100000), "$"); // Call function with salary = 1000 and increment = 500
 }
